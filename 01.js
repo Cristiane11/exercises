fa34a862a12eb1cd8f595c9271b0f639bf6e2026
Cristiane11 <bjwmcd@yahoo.com>
2020-03-09 05:19:13 -0400

    update

---
+++ 01.js
@@ -0,0 +1,8 @@
+//Write a JavaScript program to display the current day and time in the following format
+var today = getDay();
+var currentTime = getHours();
+var minutes= getMinutes();
+var secundos= getSeconds();
+
+console.log(`'Today is :' + today +  currentTime + ' ':+ minutes' ' +secundos `);
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # exercises
 html js and css
+
+https://www.w3resource.com/javascript-exercises/javascript-basic-exercises.php
