e66d459d46bcb5e696d8979245f86286989a6a8e
Cristiane11 <bjwmcd@yahoo.com>
2020-03-10 05:18:38 -0400

    update

--- 01.js
+++ 01.js
@@ -1,8 +1,10 @@
 //Write a JavaScript program to display the current day and time in the following format
-var today = getDay();
-var currentTime = getHours();
-var minutes= getMinutes();
-var secundos= getSeconds();
+var today = new Date();
+var day = today.getDay();
+var currentTime =today.getHours();
+var minutes= today.getMinutes();
+var secundos= today.getSeconds();
+ 
 
 var elUno = document.getElementById('uno');
 elUno.innerHTML ='<p>Today : ' + today +'</p>';
